9663672a8f33fcc992b71dac957bd9df874c5106
minihub <ryter.georg@gmail.com>
2024-11-17 13:21:31 +0100

    Update Add-Store.cmd

--- Add-Store.cmd
+++ Add-Store.cmd
@@ -7,8 +7,11 @@ for /f "tokens=6 delims=[]. " %%G in ('ver') do if %%G lss 16299 goto :version
 
 :: Enable extensions and set architecture type
 setlocal enableextensions
-set "arch=%PROCESSOR_ARCHITECTURE%"
-if /i "%arch%" equ "AMD64" set "arch=x64" || set "arch=arm64"
+if /i "%PROCESSOR_ARCHITECTURE%" equ "AMD64" (
+    set "arch=x64"
+) else (
+    set "arch=arm64"
+)
 
 :: Navigate to script directory
 cd /d "%~dp0"
@@ -18,69 +21,76 @@ if not exist "*WindowsStore*.msixbundle" goto :nofiles
 if not exist "*WindowsStore*.xml" goto :nofiles
 
 :: Set file variables
-for %%i in (*WindowsStore*.msixbundle) do set "Store=%%i"
-for %%i in (*NET.Native.Framework*.appx) do (
-    if /i "%%~ni"=="*x64*" set "Framework6X64=%%i"
-    if /i "%%~ni"=="*arm64*" set "Framework6arm64=%%i"
-)
-for %%i in (*NET.Native.Runtime*.appx) do (
-    if /i "%%~ni"=="*x64*" set "Runtime6X64=%%i"
-    if /i "%%~ni"=="*arm64*" set "Runtime6arm64=%%i"
-)
-for %%i in (*VCLibs*.appx) do (
-    if /i "%%~ni"=="*x64*" set "VCLibsX64=%%i"
-    if /i "%%~ni"=="*arm64*" set "VCLibsarm64=%%i"
-)
+for /f %%i in ('dir /b *WindowsStore*.msixbundle 2^>nul') do set "Store=%%i"
+for /f %%i in ('dir /b *NET.Native.Framework*.appx 2^>nul ^| find /i "x64"') do set "FrameworkX64=%%i"
+for /f %%i in ('dir /b *NET.Native.Framework*.appx 2^>nul ^| find /i "arm64"') do set "Frameworkarm64=%%i"
+for /f %%i in ('dir /b *NET.Native.Runtime*.appx 2^>nul ^| find /i "x64"') do set "RuntimeX64=%%i"
+for /f %%i in ('dir /b *NET.Native.Runtime*.appx 2^>nul ^| find /i "arm64"') do set "Runtimearm64=%%i"
+for /f %%i in ('dir /b *VCLibs*.appx 2^>nul ^| find /i "x64"') do set "VCLibsX64=%%i"
+for /f %%i in ('dir /b *VCLibs*.appx 2^>nul ^| find /i "arm64"') do set "VCLibsarm64=%%i"
+for /f %%i in ('dir /b *UX.Xaml*.appx 2^>nul ^| find /i "x64"') do set "UXXamlX64=%%i"
+for /f %%i in ('dir /b *UX.Xaml*.appx 2^>nul ^| find /i "arm64"') do set "UXXamlarm64=%%i"
 
 :: Check optional components
-for %%i in (StorePurchaseApp DesktopAppInstaller XboxIdentityProvider) do (
-    if exist "*%%i*.msixbundle" if exist "*%%i*.xml" (
-        for %%j in (*%%i*.msixbundle) do set "%%i=%%j"
-    )
+if exist "*StorePurchaseApp*.appxbundle" if exist "*StorePurchaseApp*.xml" (
+    for /f %%i in ('dir /b *StorePurchaseApp*.appxbundle 2^>nul') do set "PurchaseApp=%%i"
+)
+if exist "*DesktopAppInstaller*.msixbundle" if exist "*DesktopAppInstaller*.xml" (
+    for /f %%i in ('dir /b *DesktopAppInstaller*.msixbundle 2^>nul') do set "AppInstaller=%%i"
 )
 
 :: Set dependencies based on architecture
-set "DepStore=%VCLibs%Framework%Runtime%"
-set "DepPurchase=%DepStore%"
-set "DepXbox=%DepStore%"
-set "DepInstaller=%VCLibs%"
-
-if /i "%arch%"=="arm64" (
-    set "DepStore=%VCLibsarm64%,%Framework6arm64%,%Runtime6arm64%"
-    set "DepPurchase=%DepStore%"
-    set "DepXbox=%DepStore%"
-    set "DepInstaller=%VCLibsarm64%"
+if /i %arch%==x64 (
+    set "DepStore=%VCLibsX64%,%FrameworkX64%,%RuntimeX64%,%UXXamlX64%"
+    set "DepPurchase=%VCLibsX64%,%FrameworkX64%,%RuntimeX64%,%UXXamlX64%"
+    set "DepInstaller=%VCLibsX64%,%UXXamlX64%"
+) else (
+    set "DepStore=%VCLibsarm64%,%Frameworkarm64%,%Runtimearm64%,%UXXamlarm64%"
+    set "DepPurchase=%VCLibsarm64%,%Frameworkarm64%,%Runtimearm64%,%UXXamlarm64%"
+    set "DepInstaller=%VCLibsarm64%,%UXXamlarm64%"
 )
 
 :: Verify all dependencies exist
-for %%i in (%DepStore%) do if not exist "%%i" goto :nofiles
+for %%i in (%DepStore%) do (
+    if not exist "%%i" goto :nofiles
+)
 
 :: PowerShell command setup
 set "PScommand=PowerShell -NoLogo -NoProfile -NonInteractive -InputFormat None -ExecutionPolicy Bypass"
 
-:: Function to install packages
-:install_package
-set "pkg=%~1"
-set "dep=%~2"
-set "license=%~3"
-if defined pkg (
-    echo.
-    echo ============================================================
-    echo Installing %pkg%
-    echo ============================================================
-    echo.
-    1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath !pkg! -DependencyPackagePath !dep! -LicensePath !license!
-    %PScommand% Add-AppxPackage -Path !pkg!
-)
-goto :eof
-
 :: Add Microsoft Store
-call :install_package "%Store%" "%DepStore%" "Microsoft.WindowsStore_8wekyb3d8bbwe.xml"
+echo.
+echo ============================================================
+echo Installing Microsoft Store
+echo ============================================================
+echo.
+
+1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %Store% -DependencyPackagePath %DepStore% -LicensePath Microsoft.WindowsStore_8wekyb3d8bbwe.xml
+for %%i in (%DepStore%) do (
+    %PScommand% Add-AppxPackage -Path %%i
+)
+%PScommand% Add-AppxPackage -Path %Store%
 
 :: Optional components installation
-call :install_package "%PurchaseApp%" "%DepPurchase%" "Microsoft.StorePurchaseApp_8wekyb3d8bbwe.xml"
-call :install_package "%AppInstaller%" "%DepInstaller%" "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.xml"
-call :install_package "%XboxIdentity%" "%DepXbox%" "Microsoft.XboxIdentityProvider_8wekyb3d8bbwe.xml"
+if defined PurchaseApp (
+    echo.
+    echo ============================================================
+    echo Installing Store Purchase App
+    echo ============================================================
+    echo.
+    1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %PurchaseApp% -DependencyPackagePath %DepPurchase% -LicensePath Microsoft.StorePurchaseApp_8wekyb3d8bbwe.xml
+    %PScommand% Add-AppxPackage -Path %PurchaseApp%
+)
+
+if defined AppInstaller (
+    echo.
+    echo ============================================================
+    echo Installing App Installer
+    echo ============================================================
+    echo.
+    1>nul 2>nul %PScommand% Add-AppxProvisionedPackage -Online -PackagePath %AppInstaller% -DependencyPackagePath %DepInstaller% -LicensePath Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.xml
+    %PScommand% Add-AppxPackage -Path %AppInstaller%
+)
 
 goto :fin
 
@@ -90,6 +100,7 @@ echo ============================================================
 echo Error: Please run the script as Administrator
 echo ============================================================
 echo.
+echo Press any key to exit.
 pause >nul
 exit
 
@@ -99,6 +110,7 @@ echo ============================================================
 echo Error: Windows 11 24H2 (version 26100 or later) required
 echo ============================================================
 echo.
+echo Press any key to exit.
 pause >nul
 exit
 
@@ -108,4 +120,16 @@ echo ============================================================
 echo Error: Required files are missing in the current directory
 echo ============================================================
 echo.
-pause
+echo Press any key to exit.
+pause >nul
+exit
+
+:fin
+echo.
+echo ============================================================
+echo Installation Complete
+echo ============================================================
+echo.
+echo Press any key to exit.
+pause >nul
+exit
